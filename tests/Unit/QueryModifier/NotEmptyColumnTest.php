87b8e97ea0013e65a482535e8f29fdbfbb198db2
Roberto Segura - phproberto <roberto@phproberto.com>
2018-05-25 14:05:30 +0200

    [imp] NotEmptyColumn query modified

---
+++ tests/Unit/QueryModifier/NotEmptyColumnTest.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * @package     Phproberto\Joomla\Model
+ * @subpackage  Tests.Unit
+ *
+ * @copyright   Copyright (C) 2018 Roberto Segura López. All rights reserved.
+ * @license     GNU General Public License version 2 or later; http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+namespace Phproberto\Joomla\Model\Tests\Unit;
+
+defined('_JEXEC') || die;
+
+use Joomla\CMS\Factory;
+use PHPUnit\Framework\TestCase;
+use Phproberto\Joomla\Model\QueryModifier\NotEmptyColumn;
+
+/**
+ * NotEmptyColumn tests.
+ *
+ * @since   __DEPLOY_VERSION__
+ */
+class NotEmptyColumnTest extends TestCase
+{
+	/**
+	 * @test
+	 *
+	 * @return void
+	 */
+	public function modifierIsApplied()
+	{
+		$db = Factory::getDbo();
+
+		$query = $db->getQuery(true)
+			->select('*')
+			->from('#__test');
+
+		$modifier = new NotEmptyColumn($query, 'a.test');
+		$modifier->apply();
+
+		$this->assertSame(
+			1,
+			substr_count(
+				(string) $query,
+				"WHERE `a`.`test` IS NOT NULL AND `a`.`test` NOT IN ('', '0', '0000-00-00', '0000-00-00 00:00:00')"
+			)
+		);
+	}
+
+	/**
+	 * @test
+	 *
+	 * @return void
+	 */
+	public function callbackIsCalled()
+	{
+		$db = Factory::getDbo();
+
+		$query = $db->getQuery(true)
+			->select('*')
+			->from('#__test');
+
+		$executed = false;
+
+		$callback = function () use (&$executed) {
+			$executed = true;
+		};
+
+		$this->assertFalse($executed);
+
+		$this->modifier = new NotEmptyColumn($query, 'a.test', $callback);
+		$this->modifier->apply();
+
+		$this->assertTrue($executed);
+	}
+}
